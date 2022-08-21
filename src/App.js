08aec3dbf611374df951e6db8625bba2ba9e79d8
Huu Hao <huuhao2205@gmail.com>
2022-08-21 15:47:15 +0700

    Update

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ToDo from './screens/ToDo';
+import Done from './screens/Done';
+import Task from './screens/Task';
+import Splash from './screens/Splash';
+import Tutorial from './screens/Tutorial';
+
+import { Provider } from 'react-redux';
+import { Store } from './redux/store';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+const Tab = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={
+        ({ route }) => ({
+          tabBarIcon: ({ focused, size, color }) => {
+            let iconName;
+            if (route.name === 'To-Do') {
+              iconName = 'clipboard-list';
+              size = focused ? 25 : 20;
+            } else if (route.name === 'Done') {
+              iconName = 'clipboard-check';
+              size = focused ? 25 : 20;
+            } else if (route.name === 'Tutorial') {
+              iconName = 'book';
+              size = focused ? 25 : 20;
+            }
+            return (
+              <FontAwesome5
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          }
+        })
+      }
+      tabBaroptions={{
+        activeTintColor: '#0080ff',
+        inactiveTintColor: '#777777',
+        labelStyle: { fontSize: 15, fontWeight: 'bold' },
+      }}
+    >
+      <Tab.Screen name={'Tutorial'} component={Tutorial} options={{ header: () => null }} />
+      <Tab.Screen name={'To-Do'} component={ToDo} options={{ header: () => null }} />
+      <Tab.Screen name={'Done'} component={Done} options={{ header: () => null }} />
+    </Tab.Navigator>
+  );
+}
+
+const RootStack = createStackNavigator();
+
+function App() {
+  return (
+    <Provider store={Store}>
+      <NavigationContainer>
+        <RootStack.Navigator
+          initialRouteName="Splash"
+          screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: {
+              backgroundColor: '#0080ff'
+            },
+            headerTintColor: '#ffffff',
+            headerTitleStyle: {
+              fontSize: 25,
+              fontWeight: 'bold'
+            }
+          }}
+        >
+          <RootStack.Screen
+            name="Splash"
+            component={Splash}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <RootStack.Screen
+            name="My Tasks"
+            component={HomeTabs}
+          />
+          <RootStack.Screen
+            name="Task"
+            component={Task}
+          />
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
+}
+
+export default App;
